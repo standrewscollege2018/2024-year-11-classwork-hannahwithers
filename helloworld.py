f121e162681f73e45c9d504043a23e962a9471cf
hannahwithers <hannahwithers50@gmail.com>
2024-02-08 15:04:43 +1300

    updated code

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,10 @@
-#:D#
-num=int(input())
-print(num*2)
+
+'''
+print(int(input("gimme ur number bitch: "))*2)
+'''
+num = input("Please input a number: ")
+try:
+    num=float(num)
+    print(num*2)
+except ValueError:
+    print("Invalid Input. Please type a valid number.")
